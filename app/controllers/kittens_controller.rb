43a0f2147abd4794a33710062d6d27313765e37d
zoink506 <benhubble03@gmail.com>
2023-07-05 19:42:16 +1000

    Write update functionality for Kitten controller and view

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -19,16 +19,25 @@ class KittensController < ApplicationController
       flash[:notice] = "Kitten created, thanks!"
       redirect_to kitten_path(@kitten)
     else
-      flash[:alert] = "Error creating kitten"
+      flash[:alert] = "We ran into difficulty creating this kitten"
       p @kitten.errors.full_messages
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      flash[:notice] = "Kitten updated, thanks!"
+      redirect_to kitten_path(@kitten)
+    else
+      flash[:alert] = "We ran into difficulty updating this kitten"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
